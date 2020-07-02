7bdbb999f31fa44bd458cf3dc74040071d84a178
Jared McKinney <jaredmckinney@Jareds-MacBook-Pro.local>
2020-07-01 21:35:17 -0400

    Updates

---
+++ precache-manifest.fc9f19b8d987a0b42a3c5f24f0c455ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c40aa775caa91728aab778f666508c6",
+    "url": "/Unit-Conversion/index.html"
+  },
+  {
+    "revision": "4d70044f30b61d4d938a",
+    "url": "/Unit-Conversion/static/css/main.266773c5.chunk.css"
+  },
+  {
+    "revision": "982503cf1a737b0fd74c",
+    "url": "/Unit-Conversion/static/js/2.f4944b40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Unit-Conversion/static/js/2.f4944b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d70044f30b61d4d938a",
+    "url": "/Unit-Conversion/static/js/main.500c42e9.chunk.js"
+  },
+  {
+    "revision": "3c7832e2b45e3009fc68",
+    "url": "/Unit-Conversion/static/js/runtime-main.c4f22fe4.js"
+  }
+]);
